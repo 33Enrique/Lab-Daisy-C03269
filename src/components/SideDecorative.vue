c6ecbc5be77c9fbf60cb238551e1e8b9a0759e33
33Enrique <enrique.gomezdu@gmail.com>
2025-06-26 01:04:39 -0600

    Agregando las features requeridas para la tarea #2

--- src/components/SideDecorative.vue
+++ src/components/SideDecorative.vue
@@ -2,16 +2,17 @@
   <div class="w-80 min-h-screen bg-base-200 p-4">
     <ul class="menu text-base-content">
       <li class="menu-title">
-        <span class="text-lg text-white">Proyectos</span>
+        <span class="text-lg font-bold text-gray-800">Proyectos</span>
         <span class="text-sm opacity-50">
           {{ projects.length === 0 ? 'No hay proyectos' : '' }}
         </span>
       </li>
 
-      <li v-for="project in projects" :key="project.id">
+      <li v-for="project in projects" :key="project.id" class="flex items-center gap-2 group">
         <details v-if="project.tasks.length > 0">
           <summary @click="selectProject(project.id)" class="cursor-pointer">
-            {{ project.name }}
+            <span v-if="!editingProjectId || editingProjectId !== project.id" @dblclick="startEditProject(project)">{{ project.name }}</span>
+            <input v-else v-model="editProjectNameValue" @keyup.enter="saveEditProject(project)" @blur="cancelEditProject" class="input input-sm w-32" />
           </summary>
           <ul>
             <li v-for="task in project.tasks" :key="task.id">
@@ -20,9 +21,8 @@
           </ul>
         </details>
         <template v-else>
-          <a @click="selectProject(project.id)" class="cursor-pointer">
-            {{ project.name }}
-          </a>
+          <span v-if="!editingProjectId || editingProjectId !== project.id" @dblclick="startEditProject(project)" @click="selectProject(project.id)" class="cursor-pointer">{{ project.name }}</span>
+          <input v-else v-model="editProjectNameValue" @keyup.enter="saveEditProject(project)" @blur="cancelEditProject" class="input input-sm w-32" />
         </template>
       </li>
     </ul>
@@ -30,6 +30,24 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { useProjectsStore } from '@/stores/projects'
-const { projects, selectProject } = useProjectsStore()
+const { projects, selectProject, editProjectName, deleteProject } = useProjectsStore()
+
+const editingProjectId = ref(null)
+const editProjectNameValue = ref('')
+
+function startEditProject(project) {
+  editingProjectId.value = project.id
+  editProjectNameValue.value = project.name
+}
+function saveEditProject(project) {
+  if (editProjectNameValue.value.trim()) {
+    editProjectName(project.id, editProjectNameValue.value.trim())
+  }
+  editingProjectId.value = null
+}
+function cancelEditProject() {
+  editingProjectId.value = null
+}
 </script>
